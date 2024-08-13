645da7e7fa9836cf1c05b63bb1ff6cd3ff612dc9
metehantopcu <metehantopcu753@hotmail.com>
2024-08-13 17:24:37 +0300

    ekledim2

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-        return "Evliyim ve karıma çok aşığım <3"
-        return "EVLİYİM VE KARİMA COK ASİGİM <333"
+        return "EVLİYİM VE KARİMA COK ASİGİM<33"
         
 if __name__ == "__main__":
         app.run(debug =True)
