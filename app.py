53ddd73f4d863148b47974b081482ecb96b80e7d
metehantopcu <metehantopcu753@hotmail.com>
2024-08-13 16:59:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 def index():
         return "Evliyim ve karıma çok aşığım <3"
+        return "EVLİYİM VE KARİMA COK ASİGİM <333"
         
 if __name__ == "__main__":
         app.run(debug =True)
