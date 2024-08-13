65fe6cfa408c871911eb2f5b332153cd8f1d312e
metehantopcu <metehantopcu753@hotmail.com>
2024-08-13 14:07:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-        return "Merhaba Dünya"
+        return "Evliyim ve karıma çok aşığım <3"
         
 if __name__ == "__main__":
         app.run(debug =True)
